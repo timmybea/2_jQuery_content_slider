9c5b0c57bab32108668d380baf88c1ee34455ec8
Tim Beals <tim.beals@gmail.com>
2017-04-17 11:54:30 -0700

    An introduction to jQuery

---
+++ jQuery_slider/js/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+
+    //set options:
+    var speed = 500;
+    var autoswitch = true;
+    var autoswitch_speed = 4000;
+
+    //Add initial active class
+    $('.slide').first().addClass('active');
+
+    //hide all of the remaining slides.
+    $('.slide').hide();
+
+    $('.active').show();
+
+    $('#next').on('click', nextSlide);
+
+    $('#prev').on('click', prevSlide);
+
+    if (autoswitch == true) {
+      setInterval(nextSlide, autoswitch_speed);
+    }
+
+    //switch to next slide
+    function nextSlide() {
+       $('.active').removeClass('active').addClass('oldActive');
+
+        if($('.oldActive').is(':last-child')) {
+          $('.slide').first().addClass('active');
+        } else {
+          $('.oldActive').next().addClass('active');
+        }
+
+        $('.oldActive').removeClass('oldActive');
+        $('.slide').fadeOut(speed);
+        $('.active').fadeIn(speed);
+    }
+
+    function prevSlide() {
+      $('.active').removeClass('active').addClass('oldActive');
+
+        if($('.oldActive').is(':first-child')) {
+          $('.slide').last().addClass('active');
+        } else {
+          $('.oldActive').prev().addClass('active');
+        }
+
+        $('.oldActive').removeClass('oldActive');
+        $('.slide').fadeOut(speed);
+        $('.active').fadeIn(speed);
+    }
+});
+
+
+
